a913e78a54ae26f7e633e4ca301938cf8a5f47b1
cnohall <christopher.nohall@gmail.com>
2021-06-07 21:25:33 +0900

    navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import  TopNavbar  from './components/top-navbar';
 
 function App() {
   return (
     <div className="App">
+      <TopNavbar/>
       <header className="App-header">
         <h1>Välkommen till Fysiklabbet!</h1>
         <img src={logo} className="App-logo" alt="logo" />
+        <h3>Fysiklabbet hjälper dig som pluggar fysik på högskola eller universitet</h3>
       </header>
     </div>
   );
